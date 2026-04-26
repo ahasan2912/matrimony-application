49c2621954a4b355f23c2cb038319c29168594a8
ahasan2912 <habib15-4284@diu.edu.bd>
2026-04-26 12:54:19 +0600

    addedsfdas

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -58,11 +58,11 @@ const Navbar = () => {
     return (
         <nav ref={menuRef} className="bg-[#FFF1F3] py-2 shadow-sm sticky top-0 z-40 notranslate">
             <div className="max-w-7xl mx-auto flex items-center px-4 justify-between relative">
-                <div className="max-w-23">
+                {/* <div className="max-w-23">
                     <Link to='/metches' className="max-w-23">
                         <img src={images.logo} className='w-full h-fit' alt="applicaiton_logo" />
                     </Link>
-                </div>
+                </div> */}
                 <div className="hidden lg:flex items-center space-x-5">
                     {navLinks.map((link) => (
                         <NavLink to={link.href}
